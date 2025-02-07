9ca3b141c85641c49db28548d67e5fc517dab8cc
Azizjon Ilxomov <104157589+nosk1lljustluck@users.noreply.github.com>
2025-02-07 19:38:06 +0500

    Update LMS.cpp

--- OOP_homework_1/LMS.cpp
+++ OOP_homework_1/LMS.cpp
@@ -18,22 +18,20 @@ bool LMS::addStudentToCourse(int studentId, int courseId) {
     Student* studentPtr = nullptr;
     for (Student& s : students) {
         if (s.getId() == studentId) {
-            studentPtr = &s;
+            studentPtr = &s;  
             studentFound = true;
             break;
         }
     }
-    
     bool courseFound = false;
     Course* coursePtr = nullptr;
     for (Course& c : courses) {
         if (c.getId() == courseId) {
-            coursePtr = &c;
+            coursePtr = &c;  // Store reference to course
             courseFound = true;
             break;
         }
     }
-    
     if (studentPtr->takeCourse(*coursePtr)) {
         coursePtr->addStudent(studentId);
     }
@@ -44,11 +42,13 @@ bool LMS::addStudentToCourse(int studentId, int courseId) {
 void LMS::printDetails() {
     cout << "LMS Name: " << name << endl;
     
+    // Print students
     cout << "Students:" << endl;
     for (auto& student : students) {
         student.printDetails();
     }
 
+    // Print courses
     cout << "Courses:" << endl;
     for (auto& course : courses) {
         course.printDetails();
